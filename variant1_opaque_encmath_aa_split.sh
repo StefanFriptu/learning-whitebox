58ad3e288313510b438a65ef211389252f49ab31
Stefan Friptu <stefan.friptu@gmail.com>
2022-11-08 21:44:24 +0100

    refactor: added obfuscator script variant number

--- variant1_opaque_encmath_aa_split.sh
+++ variant1_opaque_encmath_aa_split.sh
@@ -18,5 +18,4 @@ tigress --Seed=0 --Statistics=0 --Verbosity=0 --Environment=x86_64:Linux:Gcc:4.6
     --Transform=Split \
        --SplitCount=10 \
        --Functions=aes128_enc_wb_final \
-CHOWAES.c --out=CHOWAES_OFS.c
-
+CHOWAES.c --out=variant1_CHOWAES_OFS.c
